274ece35c1d6f76edfc58a5b18c08c06dcf1a1f4
grace-jsy <sseo.oee@gmail.com>
2025-08-22 21:05:43 +0900

    feat: [array] Q3 solved

--- src/main/java/workbook2/array/Q3.java
+++ src/main/java/workbook2/array/Q3.java
@@ -8,5 +8,17 @@ public class Q3 {
         int n = sc.nextInt();
         int[] arr = new int[n];
         // TODO: arr에 n개의 정수 저장 후 역순으로 출력
+
+        // 1. 일단 1 - 5 까지의 숫자를 차례대로 출력한다.
+        // 그 다음 역순으로 출력하는 코드를 작성한다.
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = n-1; i >= 0; i--) {
+            System.out.print(arr[i]+ " ");
+        }
+
     }
 }
