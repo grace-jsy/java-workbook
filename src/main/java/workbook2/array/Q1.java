ac99ce5ed1cb3413fa5d1398fc20402187ab01e8
grace-jsy <sseo.oee@gmail.com>
2025-08-22 12:36:00 +0900

    feat: [array] Q1 solved

--- src/main/java/workbook2/array/Q1.java
+++ src/main/java/workbook2/array/Q1.java
@@ -7,5 +7,12 @@ public class Q1 {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[5];
         // TODO: arr에 5개 정수 저장 후 출력
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
