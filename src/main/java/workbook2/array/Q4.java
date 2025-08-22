69e3269fa0e508fe416a5dab99a1b38c41557f8d
grace-jsy <sseo.oee@gmail.com>
2025-08-22 21:29:57 +0900

    feat: [array] Q4 solved

--- src/main/java/workbook2/array/Q4.java
+++ src/main/java/workbook2/array/Q4.java
@@ -9,5 +9,42 @@ public class Q4 {
         int[][] B = new int[3][3];
         int[][] C = new int[3][3];
         // TODO: A, B에 값 저장 후 C = A + B 계산, C 출력
+
+        // 1. A, B 배열에 숫자를 저장한다.
+        // 2. C = A + B 계산하는 코드를 작성한다.
+        // 3. C를 출력한다.
+
+        // A 입력
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                A[i][j] = sc.nextInt();
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+
+        // B 입력
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                B[i][j] = sc.nextInt();
+            }
+        }
+
+        // C = A + B
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                C[i][j] = A[i][j] + B[i][j];
+            }
+        }
+
+        // C 출력
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(C[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
     }
 }
