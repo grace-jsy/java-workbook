e6a32b22d6787bfefbc1c0bca32c9cf53a20a187
grace-jsy <sseo.oee@gmail.com>
2025-08-31 21:13:08 +0900

    feat: [method] Q2 solved

--- src/main/java/workbook2/methods/Q2.java
+++ src/main/java/workbook2/methods/Q2.java
@@ -2,11 +2,21 @@ package main.java.workbook2.methods;
 
 import java.util.Scanner;
 
+
 public class Q2 {
     // TODO: 문자열 길이를 반환하는 stringLength 메서드 작성
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         // TODO: stringLength 호출 후 결과 출력
+        stringLength(str);
+    }
+
+    public static int stringLength(String str) {
+        int length = str.length();
+        System.out.println(length);
+        return length;
     }
 }
+
+
