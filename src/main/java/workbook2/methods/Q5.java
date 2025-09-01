6edd06efee8e64739eb1c1de77ff0f43e571305a
grace-jsy <sseo.oee@gmail.com>
2025-09-01 16:22:58 +0900

    feat: [method] Q5 solved

--- src/main/java/workbook2/methods/Q5.java
+++ src/main/java/workbook2/methods/Q5.java
@@ -9,14 +9,27 @@ public class Q5 {
         int n = sc.nextInt();
 
         String[] words = new String[n];
+
         for (int i = 0; i < n; i++) {
             words[i] = sc.next();
+//            System.out.print(words[i] + " ");
         }
-
         // TODO: reverseStrings 호출 후 결과 출력
+        String[] reversed = reverseStrings(words);
+
+        // 결과 출력
+        for (String word : reversed) {
+            System.out.print(word + " ");
+        }
     }
 
-//    public static int reverseStrings() {
-//
-//    }
+    public static String[] reverseStrings(String[] words) {
+        String[] results = new String[words.length];
+
+        for (int i = 0; i < words.length; i++) {
+            //StringBuilder로 문자열 뒤집기
+            results[i] = new StringBuilder(words[i]).reverse().toString();
+        }
+        return results;
+    }
 }
