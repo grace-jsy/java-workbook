11f8d73900391188e5b173ad43e9aa51313ab09a
grace-jsy <sseo.oee@gmail.com>
2025-09-03 10:43:08 +0900

    feat: [paramtypes] Q1 solved

--- src/main/java/workbook2/paramtypes/Q1.java
+++ src/main/java/workbook2/paramtypes/Q1.java
@@ -7,7 +7,15 @@ public class Q1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        // int result = primitiveParam(x); // TODO: primitiveParam 메서드 작성 후 주석 해제
+         int result = primitiveParam(x); // TODO: primitiveParam 메서드 작성 후 주석 해제
+
         // TODO: 원래 x와 메서드 반환 값 출력
+        System.out.println("원래 x값: " + x);
+        System.out.println("메서드 반환 x값: " + result);
+    }
+
+    public static int primitiveParam(int x) {
+        int result = x + 10;
+        return result;
     }
 }
