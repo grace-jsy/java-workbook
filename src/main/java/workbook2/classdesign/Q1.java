278d20536a69ed543266cc056b71643b3d4e39a3
grace-jsy <sseo.oee@gmail.com>
2025-09-09 20:32:28 +0900

    feat: [classdesign] Q2 solved

--- src/main/java/workbook2/classdesign/Q1.java
+++ src/main/java/workbook2/classdesign/Q1.java
@@ -1,14 +1,23 @@
 package main.java.workbook2.classdesign;
 
+import org.w3c.dom.css.Rect;
+
+import java.util.RandomAccess;
 import java.util.Scanner;
 
 class Rectangle {
     int width;
     int height;
+
     // TODO: 생성자 작성
-    int getArea() {
+    Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    int getArea(int w, int h) {
         // TODO: 넓이 반환
-        return 0;
+        return w * h;
     }
 }
 
@@ -17,7 +26,10 @@ public class Q1 {
         Scanner sc = new Scanner(System.in);
         int w = sc.nextInt();
         int h = sc.nextInt();
-        // Rectangle rect = new Rectangle(w, h); // TODO: 생성자 작성 후 주석 해제
+
+        Rectangle rect = new Rectangle(w, h); // TODO: 생성자 작성 후 주석 해제
+
         // TODO: rect.getArea() 출력
+        System.out.println(rect.getArea(w, h));
     }
 }
