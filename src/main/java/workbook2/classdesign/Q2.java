61b363c09b3b26478e9873402f5ae0cba1261449
grace-jsy <sseo.oee@gmail.com>
2025-09-09 21:13:44 +0900

    feat: [classdesign] Q3 solved

--- src/main/java/workbook2/classdesign/Q2.java
+++ src/main/java/workbook2/classdesign/Q2.java
@@ -3,17 +3,25 @@ package main.java.workbook2.classdesign;
 import java.util.Scanner;
 
 class Circle {
-    double r;
+    static double r;
+    // new를 해야 존재함
+    // 인스턴스변수
+
+    static double pi = 3.14;
+    // new를 하지 않아도 모든 객체가 참조함
+
 
     // TODO: 생성자 작성
     Circle(double r) {
         this.r = r;
     }
 
-    double getCircumference() {
+    // 클래스 메서드
+    static double getCircumference() {
         // TODO: 둘레 반환
         return 2 * Math.PI * r;
     }
+
 }
 
 public class Q2 {
@@ -24,6 +32,7 @@ public class Q2 {
          Circle c = new Circle(radius); // TODO: 생성자 작성 후 주석 해제
         // TODO: c.getCircumference() 출력
         double result = c.getCircumference();
+        Circle.getCircumference();
 
         System.out.println(result);
     }
--- src/main/java/workbook2/classdesign/Q3.java
+++ src/main/java/workbook2/classdesign/Q3.java
@@ -7,23 +7,41 @@ class Book {
     String author;
     int price;
     // TODO: 생성자 작성
+
+    Book(String title, String author, int price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
     void printInfo() {
         // TODO: 정보 출력
+        System.out.println("Title: " + title + ", Author: " + author + ", Price" + price);
     }
 }
 
 public class Q3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String t1 = sc.next();
-        String a1 = sc.next();
+
+        System.out.print("Title1: ");
+        String t1 = sc.nextLine();
+        System.out.print("Author1: ");
+        String a1 = sc.nextLine();
+        System.out.print("Price1: ");
         int p1 = sc.nextInt();
-        String t2 = sc.next();
-        String a2 = sc.next();
+        sc.nextLine();
+
+        System.out.print("Title2: ");
+        String t2 = sc.nextLine();
+        System.out.print("Author2: ");
+        String a2 = sc.nextLine();
+        System.out.print("Price2: ");
         int p2 = sc.nextInt();
-        // Book b1 = new Book(t1, a1, p1); // TODO: 생성자 작성 후 주석 해제
-        // Book b2 = new Book(t2, a2, p2); // TODO: 생성자 작성 후 주석 해제
-        // b1.printInfo(); // TODO: 생성자 작성 후 주석 해제
-        // b2.printInfo(); // TODO: 생성자 작성 후 주석 해제
+
+
+         Book b1 = new Book(t1, a1, p1); // TODO: 생성자 작성 후 주석 해제
+         Book b2 = new Book(t2, a2, p2); // TODO: 생성자 작성 후 주석 해제
+         b1.printInfo(); // TODO: 생성자 작성 후 주석 해제
+         b2.printInfo(); // TODO: 생성자 작성 후 주석 해제
     }
 }
